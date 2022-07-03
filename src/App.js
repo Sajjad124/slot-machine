e2738c0eb47dd31639797cb466b3a6c7e41e2431
Sajjad124 <181370124@gift.edu.pk>
2022-07-03 23:53:45 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+
+
+
+const App = ()=> {
+  return (
+    <div className="App">
+      <h1> Welcome to <span>Slot Machine game</span></h1>
+    </div>
+  );
+}
+
+export default App;
