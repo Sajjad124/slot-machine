20074621ea05fca6b4a15a9c68168514cbcdea6b
Sajjad124 <181370124@gift.edu.pk>
2022-07-03 23:55:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 const App = ()=> {
   return (
     <div className="App">
-      <h1> Welcome to <span>Slot Machine game</span></h1>
+      <h1 className='heading_style'> Welcome to <span >Slot Machine game</span></h1>
     </div>
   );
 }
