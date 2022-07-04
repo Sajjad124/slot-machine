e66ee4dcd92fa5ad14e550e97d6bd23ca98933e8
Sajjad124 <181370124@gift.edu.pk>
2022-07-04 15:58:57 +0500

    Props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React from "react";
 import "./App.css";
 
-const SlotM = () =>{
-  let x  = " 😄 "
-  let y  = " 😄 "
-  let z  = " 😄 "
-  if((x===y) && (y===z)){
+const SlotM = (props) => {
+  // let x = " 😄 ";
+  // let y = " 😄 ";
+  // let z = " 🎅 ";
+  if (props.x === props.y && props.y === props.z) {
     return (
       <div>
         <div className="slot_inner">
-          <h1>{x} {y} {z}</h1>
+          <h1>
+            {props}
+          </h1>
           <h1>This is matching</h1>
-          <hr/>
+          <hr />
         </div>
       </div>
-    )
+    );
+  } else {
+    return (
+      <div>
+        <div className="slot_inner">
+          <h1>
+            {props}
+          </h1>
+          <h1>This is Not matching</h1>
+          <hr />
+        </div>
+      </div>
+    );
   }
-}
+};
 
 const App = () => {
   return (
-    <div>
+    <div className="main-div">
       <div className="App">
         <h1 className="heading_style">
-          {" "}
-          Welcome to{" "}
+          Welcome to
           <span style={{ fontWeight: "bold" }}>🎰 Slot Machine game 🎰</span>
         </h1>
-        <SlotM/>
+      </div>
+      <div className="slotmachine">
+        <SlotM x = ' 😄 ' y = ' 😄 ' z = ' 😄 ' />
+        <SlotM />
+        <SlotM />
       </div>
     </div>
   );
 };
-
 export default App;
